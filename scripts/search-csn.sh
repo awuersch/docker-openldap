77f0e1e3145cff4e8f694defac47a698160e38cf
Tony Wuersch <tony.wuersch@gmail.com>
2017-12-31 01:49:55 -0500

    fix search scripts

---
+++ scripts/search-csn.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -e
+
+. ~/.ldapvars
+
+# LDAP_DOMAIN= < in .ldapvars >
+# LDAP_DN= < in .ldapvars >
+# ADMIN_USERNAME= < in .ldapvars >
+# ADMIN_PASSWORD= < in .ldapvars >
+# CONFIG_USERNAME= < in .ldapvars >
+# CONFIG_PASSWORD= < in .ldapvars >
+
+function searchcsn { # arg ...
+  ldapsearch -x -LLL "$@" -s base contextCSN entryCSN
+}
+
+echo 'ldap1 admin'
+searchcsn -H ldap://ldap1."$LDAP_DOMAIN" -D "$ADMIN_DN" -w "$ADMIN_PASSWORD" -b "$LDAP_DN"
+
+echo 'ldap2 admin'
+searchcsn -H ldap://ldap2."$LDAP_DOMAIN" -D "$ADMIN_DN" -w "$ADMIN_PASSWORD" -b "$LDAP_DN"
+
+echo 'ldap3 admin'
+searchcsn -H ldap://ldap3."$LDAP_DOMAIN" -D "$ADMIN_DN" -w "$ADMIN_PASSWORD" -b "$LDAP_DN"
+
+echo 'replica admin'
+searchcsn -H ldap://replica1."$LDAP_DOMAIN" -D "$ADMIN_DN" -w "$ADMIN_PASSWORD" -b "$LDAP_DN"
+
+echo 'replica2 admin'
+searchcsn -H ldap://replica2."$LDAP_DOMAIN" -D "$ADMIN_DN" -w "$ADMIN_PASSWORD" -b "$LDAP_DN"
+
+echo 'replica3 admin'
+searchcsn -H ldap://replica3."$LDAP_DOMAIN" -D "$ADMIN_DN" -w "$ADMIN_PASSWORD" -b "$LDAP_DN"
+
+BASE="cn=config"
+
+echo 'ldap1 config'
+searchcsn -H ldap://ldap1."$LDAP_DOMAIN" -D "$CONFIG_DN" -w "$CONFIG_PASSWORD" -b "$BASE"
+
+echo 'ldap2 config'
+searchcsn -H ldap://ldap2."$LDAP_DOMAIN" -D "$CONFIG_DN" -w "$CONFIG_PASSWORD" -b "$BASE"
+
+echo 'ldap3 config'
+searchcsn -H ldap://ldap3."$LDAP_DOMAIN" -D "$CONFIG_DN" -w "$CONFIG_PASSWORD" -b "$BASE"
+
+BASE="olcDatabase={2}mdb,cn=config"
+
+echo 'ldap1 accesslog'
+searchcsn -H ldap://ldap1."$LDAP_DOMAIN" -D "$CONFIG_DN" -w "$CONFIG_PASSWORD" -b "$BASE"
+
+echo 'ldap2 accesslog'
+searchcsn -H ldap://ldap2."$LDAP_DOMAIN" -D "$CONFIG_DN" -w "$CONFIG_PASSWORD" -b "$BASE"
+
+echo 'ldap3 accesslog'
+searchcsn -H ldap://ldap3."$LDAP_DOMAIN" -D "$CONFIG_DN" -w "$CONFIG_PASSWORD" -b "$BASE"
